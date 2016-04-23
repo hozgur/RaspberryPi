c44431a8a77530e7641adfbd83984c26bc26894a
hozgur <hakan.ozgur@gmail.com>
2016-04-23 17:01:59 +0300

    windows gpio için mock class'ı yazıldı.

--- NodeJS/Blynk/Test1/app.js
+++ NodeJS/Blynk/Test1/app.js
@@ -1,7 +1,17 @@
 
 
 var Blynk = require('blynk-library');
-var Gpio = require('onoff').Gpio;
+var os = require('os');
+var Gpio;
+if(os.platform() == 'win32')
+{
+  Gpio = require("./MockGPIO").Gpio;
+}
+else
+{
+  Gpio = require('onoff').Gpio;  
+}
+
 var AUTH = 'ff7f0bca48214a8fafef39ca27c0d637';
 var outpin = new Gpio(26, 'out');
 var blynk = new Blynk.Blynk(AUTH);
@@ -14,7 +24,7 @@ var v9 = new blynk.VirtualPin(9);
 v1.on('write', function(param)
 {
   console.log('V1:', param[0]);
-  outpin.writeSync(1);
+  outpin.writeSync(param[0]);
 });
 v9.on('read', function() {
   v9.write(new Date().getSeconds());
