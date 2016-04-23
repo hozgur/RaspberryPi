81f54213d607bb12f2488f4b39ff4511a262719d
hozgur <hakan.ozgur@gmail.com>
2016-04-23 16:23:26 +0300

    ss

--- NodeJS/Blynk/Test1/app.js
+++ NodeJS/Blynk/Test1/app.js
@@ -3,13 +3,13 @@
 var Blynk = require('blynk-library');
 var Gpio = require('onoff').Gpio;
 var AUTH = 'ff7f0bca48214a8fafef39ca27c0d637';
-
+var outpin = new Gpio(26, 'out');
 var blynk = new Blynk.Blynk(AUTH);
 
 var v1 = new blynk.VirtualPin(1);
 var v9 = new blynk.VirtualPin(9);
 
-var outpin = new Gpio(26, 'out');
+
 
 v1.on('write', function(param)
 {
